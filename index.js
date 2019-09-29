e0b70da23b097900b75edf27a465dca6eee62876
Gustav033 <55931689+Gustav033@users.noreply.github.com>
2019-09-29 12:41:35 -0300

    Index.js Atualizad

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const discord = require("discord.js")
 const client = new discord.Client();
 const config = require("./config.json")
-  
+    client.on("ready", () => {
+      client.user.setActivity("Jogos")
+      console.log(`Bot Logado Na Conta: ${client.user.username}`)
+  })
+  client.on("message", async message => {
+      if(message.author.bot) return;
+      let prefix = config.prefix
+
+      if(message.content.startsWith(prefix + "ping")){
+          let msg = await message.channel.send("Ping?")
+          let userms = msg.createdTimestamp - message.createdTimestamp
+          msg.edit("Pong, Ping:" + userms + "ms, Ping Do Bot Com Server" + Math.round(client.ping) + "ms")
+      }
+  })
 client.login(config.token)
